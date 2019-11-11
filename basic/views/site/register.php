a984721828e3a4803be53f1d012100aa4d7d7f65
Students <Students@rvt.lv>
2019-11-11 11:58:07 +0200

    dazi jauni faili

--- basic/views/site/register.php
+++ basic/views/site/register.php
@@ -20,22 +20,26 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Name *" value=""/>
+                        <input name="name" type="text" class="form-control" placeholder="Your Name *" value=""/>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Phone Number *" value=""/>
+                        <input name="phoneNum" type="text" class="form-control" placeholder="Phone Number *" value=""/>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Password *" value=""/>
+                        <input name="password" type="text" class="form-control" placeholder="Your Password *" value=""/>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Confirm Password *" value=""/>
+                        <input name="password2" type="text" class="form-control" placeholder="Confirm Password *" value=""/>
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-primary">Submit</button>
+            <button name="submitBtn" type="submit" class="btn btn-primary">Submit</button>
         </div>
     </div>
 </div>
+<?php
+    if(isset($_POST['submitBtn']))
+        echo 'test123';
+?>
